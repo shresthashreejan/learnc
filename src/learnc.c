f8fc0a34127e8ea46ba42fed173e339b4186254f
shreejan shrestha <shr.shreejan@gmail.com>
2025-02-01 13:32:36 +0545

    Static variables, functions, and pointers

--- src/learnc.c
+++ src/learnc.c
@@ -5,6 +5,11 @@
 #define FALSE 0
 #define TRUE 1
 
+int runner() {
+    static int count = 0;
+    return count++;
+}
+
 int main() {
     BOOL bool_val = TRUE;
     if(bool_val) {
@@ -29,4 +34,19 @@ int main() {
 
     strncat(first_word, last_word, available_space);
     printf("%s\n", first_word);
+
+    printf("%d ", runner());
+    printf("%d\n", runner());
+
+    // In C, functions are global by default, but static functions are scoped to the file in which they are defined.
+
+    int a = 1;
+    int * a_pointer = &a;
+
+    printf("Value of a: %d\n", a);
+    printf("Value stored in the address pointed by a_pointer: %d\n", *a_pointer);
+    printf("Address of a: %p\n", (void *)&a);
+    printf("Value of a_pointer: %p\n", (void *)a_pointer);
+    printf("Size of a: %d bytes\n", (int)sizeof(a));
+    printf("Size of a_pointer: %d bytes\n", (int)sizeof(a_pointer));
 }
